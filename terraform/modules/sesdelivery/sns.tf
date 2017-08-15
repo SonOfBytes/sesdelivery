76477734ab45ed376878d6cf215509cb85d33685
SonOfBytes <SonOfBytes@users.noreply.github.com>
2017-08-15 09:09:52 +0100

    lambda now triggered by sns

--- terraform/modules/sesdelivery/sns.tf
+++ terraform/modules/sesdelivery/sns.tf
@@ -8,3 +8,9 @@ resource "aws_sns_topic_subscription" "sesdelivery_sqs" {
   topic_arn = "${aws_sns_topic.sesdelivery_notices.arn}"
   raw_message_delivery = true
 }
+
+resource "aws_sns_topic_subscription" "sesdelivery_lambda" {
+  endpoint = "${aws_lambda_function.sesdelivery_lambda.arn}"
+  protocol = "lambda"
+  topic_arn = "${aws_sns_topic.sesdelivery_notices.arn}"
+}
